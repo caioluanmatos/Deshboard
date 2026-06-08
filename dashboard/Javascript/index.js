9f5b0dc71377f1b91e63e76c716cf3bcc1a0c087
Caio Luan <157083928+caioluanmatos@users.noreply.github.com>
2026-06-08 14:44:07 -0300

    V2
    V2.1

---
+++ dashboard/Javascript/index.js
@@ -0,0 +1,48 @@
+// Graficos //
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
+        datasets: [{
+            label: 'Vendas',
+            data: [12, 19, 8, 15, 10, 20],
+            backgroundColor: 'greenyellow',
+            borderColor: 'greenyellow',
+            borderWidth: 1,
+            borderRadius: 8
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                labels: {
+                    color: 'white'
+                }
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: 'white'
+                },
+                grid: {
+                    color: '#333'
+                }
+            },
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    color: 'white'
+                },
+                grid: {
+                    color: '#333'
+                }
+            }
+        }
+    }
+});
+    //fim de Graficos //
+
